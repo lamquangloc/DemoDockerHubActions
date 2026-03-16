d1df64f0ffbf3bbeb9ad927011f3f046c9e38fae
LamQuangLoc-dev <lamquangloc81@gmail.com>
2026-03-16 14:42:50 +0700

    feat: first commit

---
+++ src/main/java/com/example/lamquangloc/LamquanglocApplication.java
@@ -0,0 +1,13 @@
+package com.example.lamquangloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamquanglocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LamquanglocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lamquangloc/LamquanglocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lamquangloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LamquanglocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
